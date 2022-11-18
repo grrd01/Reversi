c5a145caf5a4ea834003ac556459fff21b86e899
Gerard Tyedmers <gtyedmers@gmx.net>
2022-11-18 17:08:19 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 /**
- * grrd's Memo
- * Copyright (c) 2020 Gerard Tyedmers, grrd@gmx.net
+ * grrd's Reversi
+ * Copyright (c) 2022 Gerard Tyedmers, grrd@gmx.net
  * @license MPL-2.0
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
