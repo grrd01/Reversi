a3834c3ee2e91cd3784c997ccdcaedfe761d89f8
U109281 <U109281@mobi.ch>
2023-05-01 22:20:49 +0200

    gruntfile fixed for gh-action

--- Gruntfile.js
+++ Gruntfile.js
@@ -13,7 +13,7 @@ module.exports = function(grunt) {
                     }
                 },
                 files: {
-                    'dist/Reversi/scripts/app.js': ['scripts/app.js']
+                    'dist/scripts/app.js': ['scripts/app.js']
                 }
             },
             two: {
@@ -25,7 +25,7 @@ module.exports = function(grunt) {
                     }
                 },
                 files: {
-                    'dist/Reversi/sw.js': ['sw.js']
+                    'dist/sw.js': ['sw.js']
                 }
             }
         },
@@ -38,31 +38,31 @@ module.exports = function(grunt) {
             },
             dist: {
                 files: [
-                    {'dist/Reversi/images/2online.svg': 'images/2online.svg'},
-                    {'dist/Reversi/images/2player.svg': 'images/2player.svg'},
-                    {'dist/Reversi/images/4inarow.svg': 'images/4inarow.svg'},
-                    {'dist/Reversi/images/bulp.svg': 'images/bulp.svg'},
-                    {'dist/Reversi/images/design.svg': 'images/design.svg'},
-                    {'dist/Reversi/images/dice.svg': 'images/dice.svg'},
-                    {'dist/Reversi/images/down.svg': 'images/down.svg'},
-                    {'dist/Reversi/images/easy.svg': 'images/easy.svg'},
-                    {'dist/Reversi/images/hard.svg': 'images/hard.svg'},
-                    {'dist/Reversi/images/icon.svg': 'images/icon.svg'},
-                    {'dist/Reversi/images/info.svg': 'images/info.svg'},
-                    {'dist/Reversi/images/language.svg': 'images/language.svg'},
-                    {'dist/Reversi/images/mail.svg': 'images/mail.svg'},
-                    {'dist/Reversi/images/medium.svg': 'images/medium.svg'},
-                    {'dist/Reversi/images/memo.svg': 'images/memo.svg'},
-                    {'dist/Reversi/images/ok.svg': 'images/ok.svg'},
-                    {'dist/Reversi/images/player.svg': 'images/player.svg'},
-                    {'dist/Reversi/images/puzzle.svg': 'images/puzzle.svg'},
-                    {'dist/Reversi/images/settings.svg': 'images/settings.svg'},
-                    {'dist/Reversi/images/sound_on.svg': 'images/sound_on.svg'},
-                    {'dist/Reversi/images/stone_black.svg': 'images/stone_black.svg'},
-                    {'dist/Reversi/images/stone_empty.svg': 'images/stone_empty.svg'},
-                    {'dist/Reversi/images/stone_white.svg': 'images/stone_white.svg'},
-                    {'dist/Reversi/images/tictactoe.svg': 'images/tictactoe.svg'},
-                    {'dist/Reversi/images/x.svg': 'images/x.svg'}
+                    {'dist/images/2online.svg': 'images/2online.svg'},
+                    {'dist/images/2player.svg': 'images/2player.svg'},
+                    {'dist/images/4inarow.svg': 'images/4inarow.svg'},
+                    {'dist/images/bulp.svg': 'images/bulp.svg'},
+                    {'dist/images/design.svg': 'images/design.svg'},
+                    {'dist/images/dice.svg': 'images/dice.svg'},
+                    {'dist/images/down.svg': 'images/down.svg'},
+                    {'dist/images/easy.svg': 'images/easy.svg'},
+                    {'dist/images/hard.svg': 'images/hard.svg'},
+                    {'dist/images/icon.svg': 'images/icon.svg'},
+                    {'dist/images/info.svg': 'images/info.svg'},
+                    {'dist/images/language.svg': 'images/language.svg'},
+                    {'dist/images/mail.svg': 'images/mail.svg'},
+                    {'dist/images/medium.svg': 'images/medium.svg'},
+                    {'dist/images/memo.svg': 'images/memo.svg'},
+                    {'dist/images/ok.svg': 'images/ok.svg'},
+                    {'dist/images/player.svg': 'images/player.svg'},
+                    {'dist/images/puzzle.svg': 'images/puzzle.svg'},
+                    {'dist/images/settings.svg': 'images/settings.svg'},
+                    {'dist/images/sound_on.svg': 'images/sound_on.svg'},
+                    {'dist/images/stone_black.svg': 'images/stone_black.svg'},
+                    {'dist/images/stone_empty.svg': 'images/stone_empty.svg'},
+                    {'dist/images/stone_white.svg': 'images/stone_white.svg'},
+                    {'dist/images/tictactoe.svg': 'images/tictactoe.svg'},
+                    {'dist/images/x.svg': 'images/x.svg'}
                 ]
             }
         },
@@ -75,7 +75,7 @@ module.exports = function(grunt) {
                     expand: true,
                     cwd: 'images/',
                     src: ['**/*.{png,jpg,gif}'],
-                    dest: 'dist/Reversi/images'
+                    dest: 'dist/images'
                 }]
             }
         },
@@ -85,7 +85,7 @@ module.exports = function(grunt) {
                     banner: "/*\n* grrd's Reversi\n* Copyright (c) 2022 Gerard Tyedmers, grrd@gmx.net\n* Licensed under the MPL-2.0 License\n*/\n"
                 },
                 files: {
-                    'dist/Reversi/styles/app.css': ['styles/app.css']
+                    'dist/styles/app.css': ['styles/app.css']
                 }
             }
         },
@@ -115,17 +115,17 @@ module.exports = function(grunt) {
                     ]
                 },
                 files: [
-                    {expand: true, flatten: true, src: ['dist/Reversi/index.html'], dest: 'dist/Reversi/'}
+                    {expand: true, flatten: true, src: ['dist/index.html'], dest: 'dist/'}
                 ]
             }
         },
         copy: {
             main: {
                 files: [
-                    {expand: true, flatten: true, src: ['manifest/*'], dest: 'dist/Reversi/manifest/'},
-                    {expand: true, flatten: true, src: ['**.txt'], dest: 'dist/Reversi/'},
-                    {expand: true, flatten: true, src: ['**.md'], dest: 'dist/Reversi/'},
-                    {expand: true, flatten: true, src: ['sounds/*'], dest: 'dist/Reversi/sounds/'},
+                    {expand: true, flatten: true, src: ['manifest/*'], dest: 'dist/manifest/'},
+                    {expand: true, flatten: true, src: ['**.txt'], dest: 'dist/'},
+                    {expand: true, flatten: true, src: ['**.md'], dest: 'dist/'},
+                    {expand: true, flatten: true, src: ['sounds/*'], dest: 'dist/sounds/'},
                     {expand: true, flatten: true, src: ['CNAME'], dest: 'dist/'}
                 ]
             }
